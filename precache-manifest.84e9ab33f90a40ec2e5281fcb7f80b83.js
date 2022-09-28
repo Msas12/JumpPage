d34fafdb38b5debbbcc24977d6179b2b4d887e94
Mike Sasiela <m5455y@gmail.com>
2022-09-28 17:42:56 -0500

    Updates

---
+++ precache-manifest.84e9ab33f90a40ec2e5281fcb7f80b83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a584898e63657efb6bd",
+    "url": "/JumpPage/static/css/main.cd92bc0d.chunk.css"
+  },
+  {
+    "revision": "3a584898e63657efb6bd",
+    "url": "/JumpPage/static/js/main.3a584898.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "/JumpPage/static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "7ccd5bea02597a4f8cec",
+    "url": "/JumpPage/static/css/2.0d887bec.chunk.css"
+  },
+  {
+    "revision": "7ccd5bea02597a4f8cec",
+    "url": "/JumpPage/static/js/2.7ccd5bea.chunk.js"
+  },
+  {
+    "revision": "4e60edc8a3adf63fa3a0",
+    "url": "/JumpPage/static/js/runtime~main.4e60edc8.js"
+  },
+  {
+    "revision": "1b686464151a8f923f2b35cc8a21858d",
+    "url": "/JumpPage/index.html"
+  }
+];
