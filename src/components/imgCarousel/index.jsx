075859f3d75cbb8a1e4e33b55d70a35e1261b8c9
Mike Sasiela <m5455y@gmail.com>
2022-09-29 12:22:27 -0500

    added container to carousel for desktop version

--- src/components/imgCarousel/index.jsx
+++ src/components/imgCarousel/index.jsx
@@ -1,76 +1,81 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
 
 function PhotoCarousel() {
   return (
-    <Carousel variant="dark">
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/1.svg"
-          alt="First slide"
-        />
-      </Carousel.Item>
+    <Container>
+        <Carousel variant="dark">
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/1.svg"
+                alt="First slide"
+                />
+            </Carousel.Item>
 
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/2.svg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/2.svg"
+                alt="Second slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/3.svg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/3.svg"
+                alt="Third slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/4.svg"
-          alt="Fourth slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/4.svg"
+                alt="Fourth slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/5.svg"
-          alt="Fifth slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/5.svg"
+                alt="Fifth slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/6.svg"
-          alt="Sixth slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/6.svg"
+                alt="Sixth slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/9.svg"
-          alt="Seventh slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/9.svg"
+                alt="Seventh slide"
+                />
+            </Carousel.Item>
 
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100"
-          src="imgs/10.svg"
-          alt="Eighth slide"
-        />
-      </Carousel.Item>
+            <Carousel.Item interval={1500}>
+                <img
+                className="d-block w-100"
+                src="imgs/10.svg"
+                alt="Eighth slide"
+                />
+            </Carousel.Item>
 
 
-    </Carousel>
+        </Carousel>
+
+    </Container>
+    
   );
 }
 
